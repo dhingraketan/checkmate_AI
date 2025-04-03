7cead30e350afeb6662cd8d620ba72baeb3d0fb2
Bhakti <bsbhakti01@gmail.com>
2025-04-03 02:21:27 -0700

    Made function to change color. Not tested.

--- checkmate_ai/app/src/logic_led_manager.c
+++ checkmate_ai/app/src/logic_led_manager.c
@@ -10,68 +10,82 @@ static int possible[8][8];
 static int colorArr[128];
 static pthread_t threadId;
 
-static void LogicLedManager_writeColorArr(){
+static void LogicLedManager_writeColorArr(LIGHT_UP *leds){
     assert(isInit);
 
     bool isValid = chessHelper_getIsValidMove();
 
     int indx = 0;
+    int colors[NEO_NUM_LEDS] = {0};
 
-    if(isValid){
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                if(possible[i][j]){
-                    colorArr[indx++] = COLOR_WHITE;
-                }
-                else {
-                    colorArr[indx++] = COLOR_NONE;
-                }
-                colorArr[indx++] = COLOR_NONE;
-            }
+    if(leds != NULL){
+        int count = sizeof(leds)/sizeof(leds[0]);
+
+        for(int i = 0; i<count; i++){
+            int ledIndx = (((8 * leds[i].row) + leds[i].col) *2 )- 2;
+            colors[ledIndx] = leds[i].colorName;
         }
     }
-    else {
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                colorArr[indx++] = COLOR_RED;
-                colorArr[indx++] = COLOR_NONE;
-            }
-        }
-    }
+    memcpy(colorArr, colors, sizeof(colorArr));
 }
 
-static void LogicLedManager_changeColor(){
+
+
+//     if(isValid){
+//         for(int i = 0; i < 8; i++){
+//             for(int j = 0; j < 8; j++){
+//                 if(possible[i][j]){
+//                     colorArr[indx++] = COLOR_WHITE;
+//                 }
+//                 else {
+//                     colorArr[indx++] = COLOR_NONE;
+//                 }
+//                 colorArr[indx++] = COLOR_NONE;
+//             }
+//         }
+//     }
+//     else {
+//         for(int i = 0; i < 8; i++){
+//             for(int j = 0; j < 8; j++){
+//                 colorArr[indx++] = COLOR_RED;
+//                 colorArr[indx++] = COLOR_NONE;
+//             }
+//         }
+//     }
+// }
+
+void LogicLedManager_changeColor(LIGHT_UP *leds){
     assert(isInit);
 
-    LogicLedManager_writeColorArr();
+    LogicLedManager_writeColorArr(leds);
     led_changeLedColor(colorArr);
 }
 
-void * LogicLedManager_makeThread(){
-    assert(isInit);
+// void * LogicLedManager_makeThread(){
+//     assert(isInit);
     
-    while(1){
-        pthread_mutex_lock(&ledMutex);
+//     while(1){
+//         pthread_mutex_lock(&ledMutex);
 
-        while (!isChangeLed) {
-            pthread_cond_wait(&ledCondVar, &ledMutex);
-        }
+//         while (!isChangeLed) {
+//             pthread_cond_wait(&ledCondVar, &ledMutex);
+//         }
 
-        isChangeLed = false;
+//         isChangeLed = false;
 
-        copyPossibleMoves(possible);
-        LogicLedManager_changeColor();
-        pthread_mutex_unlock(&ledMutex);
+//         copyPossibleMoves(possible);
+//         LogicLedManager_changeColor(LIGHT_UP *leds);
+//         pthread_mutex_unlock(&ledMutex);
 
-    }
-    return NULL;
-}
+//     }
+//     return NULL;
+// }
 
 
 void LogicLedManager_init(){
     isInit = true;
     led_init();
-    pthread_create(&threadId,NULL, LogicLedManager_makeThread, NULL);
+    // pthread_create(&threadId,NULL, LogicLedManager_makeThread, NULL);
 }
 
 
@@ -81,5 +95,5 @@ void LogicLedManager_cleanup(){
 
     isInit = false;
     led_cleanup();
-    pthread_join(threadId, NULL);
+    // pthread_join(threadId, NULL);
 }
